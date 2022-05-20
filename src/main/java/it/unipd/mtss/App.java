a12034a21eeaf299cea22c17a7767351b6126ca2
BackToFrancesco <bacchin.francesco2000@gmail.com>
2022-05-20 18:06:34 +0200

    close #1 close #2 close #3

---
+++ src/main/java/it/unipd/mtss/App.java
@@ -0,0 +1,17 @@
+////////////////////////////////////////////////////////////////////
+// [Lorenzo] [Onelia] [1226323]
+// [Francesco] [Bacchin] [1227269]
+////////////////////////////////////////////////////////////////////
+package it.unipd.mtss;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
