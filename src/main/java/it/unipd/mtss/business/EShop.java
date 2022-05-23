6ac5d12f8f5cef52d0922f9ab950ee7f019e2caa
Lorenzo <lorenzo.onelia@studenti.unipd.it>
2022-05-23 10:38:02 +0200

    close #6

--- src/main/java/it/unipd/mtss/business/EShop.java
+++ src/main/java/it/unipd/mtss/business/EShop.java
@@ -32,7 +32,42 @@ public class EShop implements Bill {
             throw new BillException("Nessun item nell'ordine");
         }
 
+        double processorDiscount = getProcessorDiscount(itemsOrdered);
+
+        total = total + processorDiscount;
+
         return total;
+    }
+
+    //Se vengono ordinati più di 5 Processori viene fatto uno sconto del 
+    //50% sul prezzo del Processori meno caro;
+    private double getProcessorDiscount(List<EItem> itemsOrdered) {
+        int processorNumber = 0 ;
+        double minPrice = 0;
+        for (EItem eItem : itemsOrdered) {
+            if (eItem.getEItemType() == EItemType.PROCESSOR){
+                processorNumber++;
+            }
+
+            if(processorNumber > 5){
+                EItem min = itemsOrdered.get(0);
+                minPrice = itemsOrdered.get(0).getPrice();
+                for (EItem e : itemsOrdered) {
+                    if (e.getEItemType() == EItemType.PROCESSOR){
+                         if (e.getPrice() < min.getPrice()){
+                            min = e;
+                            minPrice = e.getPrice();
+                        }
+                    }
+                }
+            }
+        }
+        if(minPrice==0){
+            return 0;
+        }
+        else{
+            return (-minPrice + (minPrice/2));
+        }
     }
 
 
